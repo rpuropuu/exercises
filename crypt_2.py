80a8ea1efa0b6503d06b36b4b6f608b9c8cfd380
rpuropuu <de.rpuropuu@gmail.com>
2021-03-11 23:10:14 +0700

    crypt_2.py done (alt_version)

---
+++ crypt_2.py
@@ -0,0 +1,14 @@
+string = 'sssqqqqqffffss'
+currCh = string[:1]
+currCount = 1
+result = ''
+for ch in string[1:]:
+    if ch != currCh:
+        result += (currCh + str(currCount))
+        currCh = ch
+        currCount = 1
+    else:
+        currCount += 1
+result += (currCh + str(currCount))
+
+print(result)
