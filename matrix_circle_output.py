c8b69d9d75fe8ca355a0d8e92df1eee94a292336
rpuropuu <de.rpuropuu@gmail.com>
2021-03-17 19:06:13 +0700

    Added matrix_circle_output.py

--- matrix_circle_output.py
+++ matrix_circle_output.py
@@ -1,24 +1,27 @@
-import numpy as np
-
-lst = []
-#n = int(input())
-n = 5
-length = n ** 2
-# генерируем матрицу нужного размера
-matrix = np.full(( n, n), 0)
-# генерируем строку с нужными элементами для вставки в матрицу
-for x in range(length):
-    lst.append(x+1)
-# переводим лист в одномерную матрицу
-arr = np.array(lst)
-
-# заполняем первую строку
-for x in range(n):
-    matrix[0,x] = arr[x]
-
-# выводим матрицу в строки
-for z in range(n):
-    for w in range(n):
-        print(matrix[z,w], end=' ')
-    print()
-
+n = int(input())
+matrix = [[0] * n for i in range(n)]
+arr, m = 1, 0
+# аранее присваиваем значение
+# центральному элементу матрицы
+matrix[n // 2][n // 2] = n * n
+for j in range(n//2):
+# заполняем верхнюю строку
+    for i in range(n-m):
+        matrix[j][i + j] = arr
+        arr+=1
+# заполняем правого столбца
+    for i in range(j+1, n-j):
+        matrix[i][-j - 1] = arr
+        arr+=1
+# заполняем нижнюю строку
+    for i in range(j+1, n-j):
+        matrix[-j - 1][-i - 1] = arr
+        arr+=1
+# заполняем левого столбца
+    for i in range(j+1, n-(j+1)):
+        matrix[-i - 1][j] = arr
+        arr += 1
+    m += 2
+# вывод на экран
+for i in matrix:
+    print(*i)
