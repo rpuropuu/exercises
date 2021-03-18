a9aa994158e87f3409a95ca7258a7325da58a88a
rpuropuu <de.rpuropuu@gmail.com>
2021-03-18 12:47:00 +0700

    dict_some_work.py +3 other ways

--- dict_some_work.py
+++ dict_some_work.py
@@ -7,9 +7,28 @@ def update_dictionary(d, key, value):
 # обработка исключающих условий
         if 2*key is d:
             d[2*key].append(value)
-        elif (2*key is not d) and d.get(2*key)==None:
-            d[2*key]=[]
+        elif (2*key is not d) and d.get(2*key) == None:
+            d[2*key] = []
             d[2*key].append(value)
-        elif (2*key is not d) and d.get(2*key)!=None:
+        elif (2*key is not d) and d.get(2*key) != None:
             d[2*key].append(value)
     return
+
+# битовый сдвиг
+def update_dictionary2(d, key, value):
+    key <<= key not in d
+    d.setdefault(key,[]).append(value) # создаётся ключ без содержания
+
+# сложно
+def update_dictionary3(d, key, value):
+    key += key * (key not in d)
+    d[key] = d.get(key, []) + [value] # создаётся ключ без содержания
+
+# присоединения +=
+def update_dictionary4(d, key, value):
+    if key in d:
+        d[key] += [value]
+    elif 2 * key in d:
+        d[2 * key] += [value]
+    else:
+        d[2 * key] = [value]
