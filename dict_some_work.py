9f6917c115d641b76fc6f60b40bcc44f62895636
rpuropuu <de.rpuropuu@gmail.com>
2021-03-18 12:39:35 +0700

    done dict_some_work.py fix comments

--- dict_some_work.py
+++ dict_some_work.py
@@ -1,18 +1,15 @@
 def update_dictionary(d, key, value):
-    # put your python code here
+# првоерка первого условия
     if key in d:
         d[key].append(value)
-        #print('ключ есть')
+# проверка второго условия
     elif key is not d:
-        #d[2*key]=[]
+# обработка исключающих условий
         if 2*key is d:
             d[2*key].append(value)
-            #print('ключ 2*key уже есть')
         elif (2*key is not d) and d.get(2*key)==None:
             d[2*key]=[]
             d[2*key].append(value)
-            #print('создание ключа и + новое значение списка')
         elif (2*key is not d) and d.get(2*key)!=None:
             d[2*key].append(value)
-            #print('создание ключа и + значение списка')
     return
