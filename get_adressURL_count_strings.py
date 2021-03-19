855d079e52c2a2311e703cbf2dc351c87ab3f655
rpuropuu <de.rpuropuu@gmail.com>
2021-03-19 19:41:15 +0700

    get_adressURL_count_strings.py +comments

--- get_adressURL_count_strings.py
+++ get_adressURL_count_strings.py
@@ -1,8 +1,8 @@
 import requests
 # копируем URL
 r = requests.get('https://stepic.org/media/attachments/course67/3.6.2/997.txt') # простой GET запрос
-# разбивает весь текст в список по строкам
+print(len(r.text.splitlines())) # разбивает весь текст в список по строкам
 # можно посмотреть как это выглядит:
 # print(r.text.splitlines())
-print(len(r.text.splitlines()))
+
 
